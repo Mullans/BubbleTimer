6ae900777424a2ca5da0297994d3e60309e49dc5
Sean Mullan <seanmm@mchsi.com>
2014-05-27 00:26:32 -0500

    Added touch recognition and a background image NOTE: need to switch out current background with something of my own

--- BubbleTimer/ViewController.m
+++ BubbleTimer/ViewController.m
@@ -17,13 +17,34 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    //sets background image
+    NSURL *imgUrl=[[NSURL alloc] initWithString:@"http://cdn.osxdaily.com/wp-content/uploads/2010/06/ipad-background-2.jpg"];
+    NSData *imgData = [NSData dataWithContentsOfURL:imgUrl];
+    UIImage *img = [UIImage imageWithData:imgData];
+    
+    imageView = [[UIImageView alloc] initWithImage:img];
+    
+    [self.view addSubview:imageView ];
+    [self.view sendSubviewToBack:imageView ];
+    
+    //set a gesture recognizer for single taps in the main view
+    oneFingerTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(oneTapDetected:)];
+    oneFingerTap.numberOfTapsRequired=1;
+    oneFingerTap.numberOfTouchesRequired=1;
+    [self.view addGestureRecognizer:oneFingerTap];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)oneTapDetected:(UITapGestureRecognizer *)recognizer{
+    NSLog(@"one tap detected");
+    tapPositionOneFingerTap = [oneFingerTap locationInView:self.view];
+    NSLog(@"%f, %f",tapPositionOneFingerTap.x,tapPositionOneFingerTap.y);
 }
 
 @end
